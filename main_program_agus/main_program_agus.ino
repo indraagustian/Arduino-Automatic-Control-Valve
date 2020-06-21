8c1795f5515b6bfdecdf337ac08cf1055482d7c5
indraagustian <56149654+indraagustian@users.noreply.github.com>
2020-06-22 00:30:09 +0700

    +smooth relay control
    adding smooth relay controller based on distance measurement (2 JSN 2 Relay)

--- main_program_agus/main_program_agus.ino
+++ main_program_agus/main_program_agus.ino
@@ -4,37 +4,115 @@ const int E0=2;
 const int T1=6;
 const int E1=8;
 
-
+/////////Distance measurement var/////////////////////
 const int numReadings = 10;
-int readings[numReadings];
-int readIndex = 0;              // the index of the current reading
-int total = 0;                  // the running total
-int average = 0;                // the average
+unsigned int readings[numReadings];
+unsigned int readIndex = 0;              // the index of the current reading
+unsigned int total = 0;                  // the running total
+unsigned int average = 0;                // the average
+int jarak_0, jarak_1;
 
 SimpleKalmanFilter simpleKalmanFilter0(2, 2, 0.01);
 SimpleKalmanFilter simpleKalmanFilter1(2, 2, 0.01);
+int dmin=25;
+int dmax=150;// distance Threshold;
+int dstate_0, dstate_1;
+int con_0, con_1, cof_0, cof_1;
+
+
+//relay
+const int rel0=10;
+const int rel1=11;
+
+//timer
+unsigned long time_0;
+unsigned long time_1;
+unsigned long interval_on=60;//40
+unsigned long interval_off=60;//20
+
+
 
 void setup() { 
-  
   Serial.begin(9600); // Serial monitoring 
+  //distance sensor
   Serial.println("distance,smooth,kalman");
   pinMode(T0, OUTPUT); // Initializing Trigger Output and Echo Input 
   pinMode(E0, INPUT_PULLUP);
   pinMode(T1, OUTPUT); // Initializing Trigger Output and Echo Input 
   pinMode(E1, INPUT_PULLUP);
+  //relay
+  pinMode(rel0,OUTPUT);
+  pinMode(rel1,OUTPUT);
+  digitalWrite(rel0,HIGH);
+  digitalWrite(rel1,HIGH);
 }
 
 void loop(){
- int jarak_0, jarak_1;
  jarak_0=ukur_jarak(T0,E0);
  int kalman_0=simpleKalmanFilter0.updateEstimate(jarak_0);
  jarak_1=ukur_jarak(T1,E1);
  int kalman_1=simpleKalmanFilter1.updateEstimate(jarak_1);
- Serial.print(kalman_0); //bacaan real
- Serial.print("\t");
- Serial.print(kalman_1); //bacaan real
- Serial.print("\t");
- Serial.println();
+// Serial.print(kalman_0); //bacaan real
+// Serial.print("\t");
+// Serial.print(kalman_1); //bacaan real
+// Serial.print("\t");
+// Serial.println();
+
+////////////activate relay 0
+ if(jarak_0>dmin && jarak_0<dmax){
+    cof_0=0;
+    if(dstate_0==0)con_0++;
+    if(con_0>interval_on){
+      dstate_0=1;
+      con_0=0;
+      Serial.print("on ");
+      Serial.print(dstate_0);
+      Serial.print(" - ");
+      Serial.println(kalman_0);
+      digitalWrite(rel0,LOW);
+    }
+  }
+ else if(jarak_0<=dmin || jarak_0>=dmax){
+    con_0=0;
+    if(dstate_0==1)cof_0++;
+    if(cof_0>interval_off){
+      dstate_0=0;
+      cof_0=0;
+      Serial.print("off ");
+      Serial.print(dstate_0);
+      Serial.print(" - ");
+      Serial.println(kalman_0);
+      digitalWrite(rel0,HIGH);
+    }
+ }
+
+////////////activate relay 1
+ if(jarak_1>dmin && jarak_1<dmax){
+    cof_1=0;
+    if(dstate_1==0)con_1++;
+    if(con_1>interval_on){
+      dstate_1=1;
+      con_1=0;
+      Serial.print("on ");
+      Serial.print(dstate_1);
+      Serial.print(" - ");
+      Serial.println(kalman_1);
+      digitalWrite(rel1,LOW);
+    }
+  }
+ else if(jarak_1<=dmin || jarak_1>=dmax){
+    con_1=0;
+    if(dstate_1==1)cof_1++;
+    if(cof_1>interval_off){
+      dstate_1=0;
+      cof_1=0;
+      Serial.print("off ");
+      Serial.print(dstate_1);
+      Serial.print(" - ");
+      Serial.println(kalman_1);
+      digitalWrite(rel1,HIGH);
+    }
+ }
 }
 
 int ukur_jarak(int TRIG,int ECHO){  //fungsi mengukur jarak
@@ -50,18 +128,10 @@ int ukur_jarak(int TRIG,int ECHO){  //fungsi mengukur jarak
     
     distance= distance/58; //Convert the pulse duration to distance
                            //You can add other math functions to calibrate it well
-    //int kalman_distance=simpleKalmanFilter.updateEstimate(distance); 
-                    
-    //Serial.print("Distance ");
-
-    //Serial.print(distance); //bacaan real
-    //Serial.print("\t");
-        
-    //Serial.print(kalman_distance); //bacaan dengan kalman filter  
-    //Serial.print("\t");
-    //Serial.println();
     return distance;
-    delay(50);
+    delay(100);
 }
+
+
 
 
